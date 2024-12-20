a3fef530071a6a0af3cdaa84f06d626545655ffa
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-20 23:51:22 +0530

    updated commit

--- datatype/string_slice.py/string_method.py
+++ datatype/string_slice.py/string_method.py
@@ -1,3 +1,23 @@
-chai = "masala chai"
+chai = "Lemon chai, Ginger, masala, Mint"
 print(chai.lower())
 print(chai.upper())
+
+print(chai.strip())
+print(chai.lstrip())
+print(chai.rstrip())
+
+print(chai.find("chai"))
+print(chai.replace("chai", "tea"))
+
+print(chai.split(","))
+
+print(chai.isalpha())
+print(chai.isdigit())
+
+print(chai.startswith("Lemon"))
+print(chai.endswith("Mint"))
+
+print(chai.count("a"))
+
+print(chai.index("chai"))
+print(chai.find("chai"))
