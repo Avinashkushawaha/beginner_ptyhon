179e0fe70a71dbde7d3170058b88f1ad8270e05c
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-20 23:59:34 +0530

    updated commit

--- datatype/string_slice.py/string_method.py
+++ datatype/string_slice.py/string_method.py
@@ -1,23 +1,30 @@
-chai = "Lemon chai, Ginger, masala, Mint"
-print(chai.lower())
-print(chai.upper())
+# chai = "Lemon chai, Ginger, masala, Mint"
+# print(chai.lower())
+# print(chai.upper())
 
-print(chai.strip())
-print(chai.lstrip())
-print(chai.rstrip())
+# print(chai.strip())
+# print(chai.lstrip())
+# print(chai.rstrip())
 
-print(chai.find("chai"))
-print(chai.replace("chai", "tea"))
+# print(chai.find("chai"))
+# print(chai.replace("chai", "tea"))
 
-print(chai.split(","))
+# print(chai.split(","))
 
-print(chai.isalpha())
-print(chai.isdigit())
+# print(chai.isalpha())
+# print(chai.isdigit())
 
-print(chai.startswith("Lemon"))
-print(chai.endswith("Mint"))
+# print(chai.startswith("Lemon"))
+# print(chai.endswith("Mint"))
 
-print(chai.count("a"))
+# print(chai.count("a"))
 
-print(chai.index("chai"))
-print(chai.find("chai"))
+# print(chai.index("chai"))
+# print(chai.find("chai"))
+
+chai_type = "Masala chai"
+quantity = 2
+order = "I ordered {} cups of {} chai"
+order = 'I ordered {} cups of {} chai'
+print(order.format(quantity, chai_type))
+   
