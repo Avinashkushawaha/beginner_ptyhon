c68cc52aa69a7b75aef827245e85efb3a3e209d2
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-21 00:25:49 +0530

    updated commit

--- datatype/string_slice.py/list/list.py
+++ datatype/string_slice.py/list/list.py
@@ -1,4 +1,10 @@
-chai_variety = ["Masala", "Ginger", "Cardamom", "Mint"]
+# chai_variety = ["Masala", "Ginger", "Cardamom", "Mint"]
 # print(", ".join(chai_variety))
 
-print(len(chai_variety))
+# print(len(chai_variety))
+letter = "abhisek"
+print(letter)
+
+# for let in letter:
+#     print(let)
+
