16749723515c1d47425b465c698a6ae1a608dc22
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-23 14:49:44 +0530

    updated commit

---
+++ datatype/SwapTwoVariable.py
@@ -0,0 +1,17 @@
+# x = 13
+# y = 12
+# temp = x
+# print("The value of temp variable is",temp)
+
+# x = y
+# print("The value of x is",x)
+
+# y = temp
+# print("The value of y is",y)
+
+x = 13
+y = 12
+
+x, y = y, x
+print("The value of x is",x)
+print("The value of y is",y)
