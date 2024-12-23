59e3ae8d0b8bf0ba94666366eb033dfd828befa0
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-23 14:31:36 +0530

    updated commit

--- datatype/squareRoot.py
+++ datatype/squareRoot.py
@@ -1,4 +1,10 @@
-num = 64
-num1 = int(input("Enter a number: "))
-sqrt = num1 ** 0.5
-print("The square root of %0.3f is %0.3f" %(num1, sqrt))
+# num = 64
+# num1 = int(input("Enter a number: "))
+# sqrt = num1 ** 0.5
+# print("The square root of %0.3f is %0.3f" %(num1, sqrt))
+
+import math
+num = int(input("Enter a number: "))
+sqrt = math.sqrt(num)
+print("The square root of %0.3f is %0.3f" %(num, sqrt))
+
