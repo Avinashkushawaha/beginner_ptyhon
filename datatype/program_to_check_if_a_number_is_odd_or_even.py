32cb6a58fbd9c7bfeaff0fdfa3efb36c64b9cb90
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-24 08:58:52 +0530

    first commit

---
+++ datatype/program_to_check_if_a_number_is_odd_or_even.py
@@ -0,0 +1,14 @@
+# program to check if a number is odd or even:
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print("It is an even number")
+# else:
+#     print("It is an odd number")
+
+#*************************************************
+
+
+
+
+
+        
