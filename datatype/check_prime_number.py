21141d1c462ff8210979ba339b348b96f0a91de2
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-24 21:32:25 +0530

    updated commit

--- datatype/check_prime_number.py
+++ datatype/check_prime_number.py
@@ -2,7 +2,7 @@
 num = int(input("Enter a number: "))
 if num > 1:
     for i in range (2, num):
-        if (num % i) ==0:
+        if (num % i) == 0:
             print(num, "is not a prime number")
     else:
             print(num, "is a prime number")    
