83058d64ef92169e2e2acf92483af2b6890fa8a8
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-26 01:07:20 +0530

    updated commit

--- datatype/program_to_display_the_multiplication_table.py
+++ datatype/program_to_display_the_multiplication_table.py
@@ -1,10 +1,19 @@
 # Python program to print the multiplication table of a number
-num = int(input("Enter a number: "))
-for i in range(1, 11):
+# num = int(input("Enter a number: "))
+# for i in range(1, 11):
     # print(f"{num} x {i} = {num * i}")
     # print(num, 'x', i, '=', num * i)
-      print(num, '*', i, '=', num * i)
+    #   print(num, '*', i, '=', num * i)
 
 
+#   python program to print the entire multiplication table
 
-      
+
+for i in range(1, 11):
+    for j in range(2, 11):
+        # print(f"{i} X {j} ", end="\t")
+        print(i,'X', j,'=', i * j,end="\t")
+
+    print()
+        
+    
