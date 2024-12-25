5aed80a85050124d79f6944bc071fdd04148fd41
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-25 21:38:58 +0530

    updated commit

--- datatype/Convert_celsius_to_fahrenheit.py
+++ datatype/Convert_celsius_to_fahrenheit.py
@@ -1,1 +1,6 @@
 # Python program to convert celcius to fahrenheit
+celcius = float(input("Enter temperature in celcius: "))
+
+fahrenhiet = (celcius * 9/5) + 32
+
+print(f"{celcius}°C is equal to {fahrenhiet}°F")
