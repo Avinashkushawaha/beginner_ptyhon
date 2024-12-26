f683a9ef8b1aa025996d4db4fbf4bdfc279db31c
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-27 00:32:34 +0530

    first commit

--- datatype/Print_fibonacci_sequence.py
+++ datatype/Print_fibonacci_sequence.py
@@ -1,2 +1,15 @@
 # Python Program to Print the Fibonacci sequence
+a = 0
+b = 1
+num = int(input("Enter the number of fibonacci sequence: "))
 
+if num ==  1:
+    print(a)
+else:
+    print(a)
+    print(b)
+    for i in range(2, num):
+        c = a + b
+        a = b
+        b = c
+        print(c)      
