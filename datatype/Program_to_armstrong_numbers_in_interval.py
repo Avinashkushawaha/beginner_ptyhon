dbadcee5fbfcf2f87b79e404f589cbbe3c52cdd2
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-27 01:17:43 +0530

    updated commit

--- datatype/Program_to_armstrong_numbers_in_interval.py
+++ datatype/Program_to_armstrong_numbers_in_interval.py
@@ -1,1 +1,18 @@
 # Python program to find Armstrong number in an Interval
+lower = int(input("Enter the lower limit: "))
+upper = int(input("Enter the upper limit: "))
+
+sum = 0
+temp = 0
+
+for num in range(lower, upper + 1):
+    order = len(str(num))
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** order
+        temp //= 10
+    if num == sum:
+        print(num)
+    sum = 0
+    
