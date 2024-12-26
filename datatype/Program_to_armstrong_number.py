7d85054b7fbfd36cf81b058553e807134bacafae
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-27 00:56:39 +0530

    updated commit

--- datatype/Program_to_armstrong_number.py
+++ datatype/Program_to_armstrong_number.py
@@ -1,16 +1,33 @@
 # Python Program to check Armstrong Number
+# num = int(input("Enter a number: "))
+
+# sum = 0
+# temp = num
+
+# while temp > 0:
+#     digit = temp % 10
+#     sum += digit ** 3
+#     temp //= 10
+
+# if num == sum:
+#     print(num, "is an Armstrong number")    
+# else:
+#     print(num, "is not an Armstrong number")
+
+# ***********************************
 num = int(input("Enter a number: "))
+order = len(str(num))
 
 sum = 0
 temp = num
 
 while temp > 0:
     digit = temp % 10
-    sum += digit ** 3
+    sum += digit ** order
     temp //= 10
 
 if num == sum:
-    print(num, "is an Armstrong number")    
+    print(num, "is an Armstrong number")
 else:
-    print(num, "is not an Armstrong number")
-    
+    print(num, "is not an Armstrong number")        
+
