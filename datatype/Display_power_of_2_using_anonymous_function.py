61d65274c200dfa6e43914cf66de09f3b80b5ac7
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-28 21:45:32 +0530

    updated commit

--- datatype/Display_power_of_2_using_anonymous_function.py
+++ datatype/Display_power_of_2_using_anonymous_function.py
@@ -1,3 +1,7 @@
 # Python Program to display the power of 2 using anonymous function
-nterms = int(input("Enter number of  terms? "))
+nterms = int(input("Enter number of  terms here: "))
 result = list(map(lambda x: 2 ** x, range(nterms)))
+
+
+for i in range(nterms):
+    print("2 raised to power", i, "is", result[i])
