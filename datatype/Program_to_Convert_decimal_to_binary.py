dfe12229a50d4d510d59fc22b089b3ca34a9d23c
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-29 23:09:30 +0530

    updated  commit

--- datatype/Program_to_Convert_decimal_to_binary.py
+++ datatype/Program_to_Convert_decimal_to_binary.py
@@ -1,1 +1,6 @@
 # Python Program to Convert Decimal to Binary , Octal, and Hexadecimal
+decimal = int(input("Enter a number here:"))
+
+print ("The conversion of decimal number", decimal, "is: ")
+print(bin(decimal),"in octal")
+print(hex(decimal), "in hexadecimal")
