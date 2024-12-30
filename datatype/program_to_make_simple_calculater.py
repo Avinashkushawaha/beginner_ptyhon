a2c9165a65f488a9dad7d034fb8535bc97723f79
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-30 11:32:51 +0530

    updated commit

--- datatype/program_to_make_simple_calculater.py
+++ datatype/program_to_make_simple_calculater.py
@@ -1,1 +1,19 @@
 # Python program to make a simple Calculator 
+num1 = float(input("Enter first  number here :"))
+num2 = float(input("Enter second number here :"))
+
+print("press 1 for addition \npress 2 for subtraction \npress 3 for multiplycation \npress 4 is divide")
+
+choice = int(input("Enter your choice from 1-4:"))
+
+if choice == 1:
+    print(num1 + num2)
+elif choice == 2:
+    print(num1 - num2)    
+elif choice == 3:
+    print(num1 * num2)  
+elif choice == 4:
+    print(num1 / num2)   
+else: 
+    print("Invalid input")         
+            
