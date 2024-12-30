a9ecc846de59a2d9c634636eb12e475ada011c48
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-30 11:08:54 +0530

    updated commit

--- datatype/Program_to_Find_Hcf_or_Gcd.py
+++ datatype/Program_to_Find_Hcf_or_Gcd.py
@@ -4,4 +4,9 @@ def FindHCF (x, y):
         smaller = y
     else:
         smaller = x    
-           
+    for i in range (1, smaller + 1):
+        if((x%i == 0)and (y % i ==0)):
+            hcf = i
+    return hcf
+
+print("the hcf of the given two numbers is ", FindHCF(12, 30))        
