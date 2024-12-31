ed737e9037fb8294cb0e7aee7d2cccd119d8b6e5
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-31 09:54:15 +0530

    updated commit

--- datatype/find_sum_of_natural_number.py
+++ datatype/find_sum_of_natural_number.py
@@ -1,11 +1,11 @@
 def Nnum(n):
    if n <= 1:
-      return
+      return n
    else:
-      return(n) +Nnum(n-1) 
-n = int(input("Enter a number"))
+      return (n)+Nnum(n-1) 
+n = int(input("Enter a number here :"))
 
 if n <=0:
    print("Enter a positive number:")
 else:
-   print("The sum of natural number upto given number is :")   
+   print("The sum of natural number upto given number is",Nnum(n))   
