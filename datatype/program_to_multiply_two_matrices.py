e1856609860e101554e72aa21041b55269305349
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-02 10:34:13 +0530

    updated commit

--- datatype/program_to_multiply_two_matrices.py
+++ datatype/program_to_multiply_two_matrices.py
@@ -5,3 +5,14 @@ A = [[2, 3, 4],
 B = [[1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]]
+
+result = [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]]
+
+for i in range(len(A)):
+    for j in range(len(A[0])):
+        result[i][j] = A[i][j] * B[i][j]
+
+for r in result:
+    print(r)        
