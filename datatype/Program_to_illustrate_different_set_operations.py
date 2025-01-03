773a2082a4aaf4a7eaff2b8b9bd2925c141e5479
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-03 10:45:44 +0530

    updated commit

---
+++ datatype/Program_to_illustrate_different_set_operations.py
@@ -0,0 +1,7 @@
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+print("Union of A and B is", A | B)
+print("Intersection of A and B is", A & B)
+print("Difference of A and B is", A - B)
+print("Symmetric difference of A and B is", A ^ B)
