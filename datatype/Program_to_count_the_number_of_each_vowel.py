6d815e5ce2739a6b4db95da08cebe26e17cdff5e
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-03 11:00:17 +0530

    updated commit

--- datatype/Program_to_count_the_number_of_each_vowel.py
+++ datatype/Program_to_count_the_number_of_each_vowel.py
@@ -5,8 +5,12 @@ vowels = "aeiou"
 # a = a.casefold()
 # print(a)
 
-count = {}.fromkeys(vowels, 0)
-for char in a:
-    if char in count:
-        count[char] += 1
+# count = {}.fromkeys(vowels, 0)
+# for char in a:
+#     if char in count:
+#         count[char] += 1
+# print(count)
+
+# solution 2 using dictionary
+count = {key:sum([1 for char in a if char == key]) for key in vowels}
 print(count)
