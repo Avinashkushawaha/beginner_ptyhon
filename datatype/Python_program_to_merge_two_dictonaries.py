3c14f2989c16ab1fee45fdc1dac5cb328f4ec498
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-04 22:09:00 +0530

    updated commit

--- datatype/Python_program_to_merge_two_dictonaries.py
+++ datatype/Python_program_to_merge_two_dictonaries.py
@@ -3,3 +3,10 @@ dict2 = {"john": 466, "peter": 45}
 
 print(dict1 | dict2)
 
+# Solution 2: Using ** Operator
+
+dict1 = {"john": 40, "peter": 45}
+dict2 = {"john": 466, "peter": 45}
+
+print({**dict1, **dict2})
+
