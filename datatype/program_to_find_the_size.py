3a62182e0242229f1d41de2f3ca112882a8f0fd1
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-04 21:58:46 +0530

    updated commit

--- datatype/program_to_find_the_size.py
+++ datatype/program_to_find_the_size.py
@@ -1,4 +1,8 @@
 import PIL
 from PIL import Image
+import PIL.Image
 
-img
+img = PIL.Image.open("C:/Users/DELL/Downloads/IMG_20200701_113013.jpg")
+width, height = img.size
+
+print (width,"x ", height)
