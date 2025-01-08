dec04a7661552169a6771034fe87e248207a529c
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-08 19:51:34 +0530

    updated

--- datatype/file_handling/program_first_file_handling.py
+++ datatype/file_handling/program_first_file_handling.py
@@ -1,3 +1,9 @@
 # write a program first file handling
 def get_line(file_name: str, num:int):
-    file
+    file = open(file_name)
+    lines = file.readlines()
+    line = lines[num-1]
+    file.close[ ]
+    return line
+
+print(get_line ("sample.log", 3))
