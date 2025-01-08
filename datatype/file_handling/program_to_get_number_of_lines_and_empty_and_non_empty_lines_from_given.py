33410adc1f8832d85cc097626ce29eb29d9f7a5d
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-08 20:07:34 +0530

    updated commit

--- datatype/file_handling/program_to_get_number_of_lines_and_empty_and_non_empty_lines_from_given.py
+++ datatype/file_handling/program_to_get_number_of_lines_and_empty_and_non_empty_lines_from_given.py
@@ -4,4 +4,11 @@ def get_lines_info(file_name):
     total_lines = 0
     valid_lines = 0
     blank_lines = 0
-   
+    with open (file_name)as file:
+        for line in file:
+          total_lines += 1   
+        if line.strip() !="":
+            valid_lines += 1
+        else:
+            blank_lines +=1
+            return {"total": total_lines,"valid": valid_lines, "blank": blank_lines}    
