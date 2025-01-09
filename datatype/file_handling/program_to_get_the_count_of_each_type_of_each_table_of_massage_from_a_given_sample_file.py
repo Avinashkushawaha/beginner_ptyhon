e1bfab672b0ca41dc769988332e30c5503248d9c
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-01-09 22:20:43 +0530

    updated commit

--- datatype/file_handling/program_to_get_the_count_of_each_type_of_each_table_of_massage_from_a_given_sample_file.py
+++ datatype/file_handling/program_to_get_the_count_of_each_type_of_each_table_of_massage_from_a_given_sample_file.py
@@ -1,4 +1,18 @@
 # write a program to get the count of each type of each table of massage from a given sample
 
 def get_msg_count(file_name):
-    
+    output = {}
+
+    with open(file_name, 'r') as file:
+        for line in file:
+            if line.strip():
+              words = line.split()
+              if len(words) > 2:
+                msg = words [2]
+            if msg in output:
+                output[msg] +=1
+            else:
+                output[msg] = 1
+                file.close()
+
+        return output
